477cd64f6f29309d11d2532725bb13c18bb449da
sachin-kuldeep <sachinkuldeep201@gmail.com>
2024-06-24 11:58:46 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
 
 function App() {
+  const [mode, setMode]=useState('light');
+  const [alert, setAlert]=useState(null);
+
+  const showAlert=(message, type)=>{
+    setAlert({
+        msg: message,
+        type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#192939';
+      showAlert("Dark mode enabled!", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode enabled!", "success");
+    }  
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+
+        <Alert alert={alert} />
+  
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} alert={alert} showAlert={showAlert} />} />
+
+            <Route exact path="/about" element={<About mode={mode} />} />
+          </Routes>
+        </div>
+        </Router>
+
+        
+        
+        
+
+        
     </div>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+    const handleUpclick=()=> {
+        let newText=text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to uppercase!", "success");
+    }
+
+    const handleLowclick=()=> {
+        let newText=text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to lowercase!", "success");
+    }
+
+    const handleClear=()=> {
+        let newText="";
+        setText(newText)
+        props.showAlert("Text cleared!", "success");
+    }
+
+    const handleSentClick=()=> {
+        let newText=text.charAt(0).toUpperCase() + text.slice(1);
+        setText(newText)
+        props.showAlert("Converted to sentence case!", "success");
+    }
+
+    const handleExtraSpaces=()=>{
+        let newText=text.split(/[ ]+/);
+        setText(newText.join(" "))
+        props.showAlert("Extra spaces removed!", "success");
+    }
+
+    const handleChange=(event)=> {
+        setText(event.target.value)
+    }
+
+    const [text, setText]=useState("");
+
+  return (
+    <>
+    <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleChange} id="myBox" rows="10"  style={{backgroundColor:props.mode==='dark'?'#253b50':'white', color:props.mode==='dark'?'white':'black'}}></textarea>
+      </div>
+      <button disabled={text.length===0} className="btn btn-primary mx-2" onClick={handleUpclick}>
+        Convert to Uppercase
+      </button>
+      <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleLowclick}>
+        Convert to Lowercase
+      </button>
+      <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleSentClick}>
+        Convert to Sentence case
+      </button>
+      <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleExtraSpaces}>
+        Remove extra spaces
+      </button>
+      <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleClear}>
+        Clear
+      </button>
+    </div>
+
+    <div className="container my-2" style={{color:props.mode==='dark'?'white':'black'}}>
+        <h2>Your text summary</h2>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words, {text.length} characters </p>
+        <p>{0.008*text.split(/\s+/).filter((element)=>{return element.length!==0}).length} minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+  );
+}
